6921ea9fe9ff4e330c49d796ad26a1a7151dbb0e
chp_h <veron.grigoreva@gmail.com>
2024-04-09 13:23:29 +0300

    Change methods from set to map

--- ControlJava.java
+++ ControlJava.java
@@ -8,6 +8,7 @@ public class ControlJava {
         
         // -Создать множество ноутбуков.
             Notebook notebook1 = new Notebook("Lenovo", 16, 512, "Windows 11 Home SL", "черный");
+            System.out.println(notebook1);
             Notebook notebook2 = new Notebook("Asus", 16, 512, "не установлена", "черный");
             Notebook notebook3 = new Notebook("Apple", 8, 256, "macOS", "серебристый");
             Notebook notebook4 = new Notebook("HP",  8, 512, "Windows 11 Home SL", "серебристый");
@@ -28,71 +29,180 @@ public class ControlJava {
             notebooks.add(notebook5);
             notebooks.add(notebook6);
             notebooks.add(notebook7);
-            
-            Notebook searchnote;
-            searchnote = setRequest();
-            for (Notebook note : notebooks) {
-                if(searchnote.equals(note)){
-                    System.out.println(note);
+        HashMap<Integer, Object> filter = searchNote();
+        HashMap<Integer, Notebook> result = (findNote(filter, notebooks));
+        if (result.isEmpty()) {
+            System.out.println("Нет подходящих ноутбуков.");
+        } else {
+            System.out.println(result.values());
+        }
+
+        scanner.close();
+    }
+
+    // создаем коллекцию Map из критериев, которые интересуют пользователя
+    public static HashMap<Integer, Object> searchNote() {
+        HashMap<Integer, Object> search = new HashMap<>();
+        System.out.println("Введите запросы по критериям, если критерий неважен, поставьте 0");
+        search.put(1, getSearchBrand());// методы, запрашивающие критерии, ниже
+        search.put(2, getSearchRam());
+        search.put(3, getSearchSdd());
+        search.put(4, getSearchOs());
+        search.put(5, getSearchColor());
+        return search;
+    }
+
+    // создаем метод, который сравнивает существующие ноутбуки с запросом
+    public static HashMap<Integer, Notebook> findNote(HashMap<Integer, Object> search, HashSet<Notebook> notebooks) {
+        HashMap<Integer, Notebook> findNote = new HashMap<>();
+        int mapKey = 1;
+        for (Notebook notebook : notebooks) {
+            boolean isFind = true;
+            for (Map.Entry<Integer, Object> entry : search.entrySet()) {
+                int key = entry.getKey();
+                if (key == 1) {
+                    String brand = entry.getValue().toString();
+                    if (!notebook.getBrand().equals(brand)) {
+                        isFind = false;
+                        System.out.println(brand);
+                    }
+                } else if (key == 2) {
+                    int ram = (int) entry.getValue();
+                    if (notebook.getRam() < ram) {
+                        isFind = false;
+                    }
+                } else if (key == 3) {
+                    int sdd = (int) entry.getValue();
+                    if (notebook.getSdd() < sdd) {
+                        isFind = false;
+                    }
+                } else if (key == 4) {
+                    String os = entry.getValue().toString();
+                    if (!notebook.getOs().equals(os)) {
+                        isFind = false;
+                    }
+                } else {
+                    String color = entry.getValue().toString();
+                    if (!notebook.getColor().equals(color)) {
+                        isFind = false;
+                    }
+                }
+                if(isFind == false){
+                    break;
                 }
             }
-            scanner.close();
+            if (isFind = true) {
+                findNote.put(mapKey, notebook);
+                mapKey++;
+            }
+
+        }
+        System.out.println(findNote);
+        return findNote;
 
     }
 
-// Создаем ноутбук по запросу,  с которым будем сравнивать существующие ноутбуки
-    
-
-    public static Notebook setRequest() {
-        System.out.println("Введите запрашиваемые критерии, если критерий неважен, введите 0");
-        Notebook searchNotebook = new Notebook(getSearchBrand(), getSearchRam(), getSearchSdd(), getSearchOs(), getSearchColor());
-        return searchNotebook;
-    }
-
+    // создаем методы, запращивающие критерии:
     public static String getSearchBrand() {
         System.out.println("Введите название бренда:");
         String input = scanner.nextLine();
-        if(input == "0"){
-            return null;
-        }
-        else return input;
+        return input;
     }
+
     public static Integer getSearchRam() {
-        System.out.println("Введите минимальный объем оперативной памяти:");
+        System.out.println("Введите минимальный объем оперативной памяти: ");
         int input = scanner.nextInt();
-        if(input == 0){
-            return null;
-        }
-        else return input;
+        return input;
     }
 
     public static Integer getSearchSdd() {
-        System.out.println("Введите минимальный объем жесткого диска:");
+        System.out.println("Введите минимальный объем жесткого диска: ");
         int input = scanner.nextInt();
-        if(input == 0){
-            return null;
-        }
-        
-        else return input;
+        scanner.nextLine();
+        return input;
     }
+
     public static String getSearchOs() {
         System.out.println("Введите операционную систему:");
-        String input = scanner.nextLine();
-        
-        if(input == "0"){
-            return null;
-        }
-        else return input;
+        String input = scanner.next();
+        scanner.nextLine();
+
+        return input;
     }
 
     public static String getSearchColor() {
         System.out.println("Введите цвет:");
-        String input = scanner.nextLine();
-        if(input == "0"){
-            return null;
-        }
-        else return input;
+        String input = scanner.next();
+        return input;
     }
-
-   }
-
+}
+            
+//            Notebook searchnote;
+//            searchnote = setRequest();
+//            for (Notebook note : notebooks) {
+//                if(searchnote.equals(note)){
+//                    System.out.println(note);
+//                }
+//                else System.out.println("Ноутбук не найден");
+//            }
+//            scanner.close();
+//    }
+//
+//// Создаем ноутбук по запросу,  с которым будем сравнивать существующие ноутбуки
+//
+//
+//    public static Notebook setRequest() {
+//        System.out.println("Введите запрашиваемые критерии, если критерий неважен, введите 0");
+//        Notebook searchNotebook = new Notebook(getSearchBrand(), getSearchRam(), getSearchSdd(), getSearchOs(), getSearchColor());
+//        System.out.println(searchNotebook);
+//        return searchNotebook;
+//    }
+//
+//    public static String getSearchBrand() {
+//        System.out.println("Введите название бренда:");
+//        String input = scanner.nextLine();
+//        if(input == "0"){
+//            return null;
+//        }
+//        else return input;
+//    }
+//    public static Integer getSearchRam() {
+//        System.out.println("Введите минимальный объем оперативной памяти:");
+//        int input = scanner.nextInt();
+//        if(input == 0){
+//            return null;
+//        }
+//        else return input;
+//    }
+//
+//    public static Integer getSearchSdd() {
+//        System.out.println("Введите минимальный объем жесткого диска:");
+//        int input = scanner.nextInt();
+//        if(input == 0){
+//            return null;
+//        }
+//
+//        else return input;
+//    }
+//
+//    public static String getSearchOs() {
+//        System.out.println("Введите операционную систему:");
+//        String input = scanner.nextLine();
+//        if(input == "0"){
+//            return null;
+//        }
+//        else return input;
+//    }
+//
+//    public static String getSearchColor() {
+//        System.out.println("Введите цвет:");
+//        String input = scanner.nextLine();
+//        if(input == "0"){
+//            return null;
+//        }
+//        else return input;
+//    }
+//
+//
+//   }
+//
